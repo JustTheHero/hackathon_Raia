fcca09638d9651bdcc89b724f4b174ca9805753c
Juan Marques <114316462+JustTheHero@users.noreply.github.com>
2025-09-20 15:28:11 -0300

    api integration

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from isso import test_enhanced_model
+
+app = FastAPI()
+
+API_KEY = ""
+
+@app.get("/predict")
+def run_prediction():
+    report_filename = test_enhanced_model(openai_api_key=API_KEY)
+
+    return FileResponse(
+        report_filename,
+        media_type="text/plain",
+        filename="report.txt"
+    )
